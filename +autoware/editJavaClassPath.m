73813b01e0ddbe10b5fa5d8993a623b123b9d867
nxt-norick <noriyuki_ota@nexty-ele.com>
2019-04-16 19:25:09 +0900

    Fix the path of jar file

--- +autoware/editJavaClassPath.m
+++ +autoware/editJavaClassPath.m
@@ -8,11 +8,12 @@ function [] = editJavaClassPath()
 % 
 
 mfile_path = fileparts(mfilename('fullpath'));
-custom_msgs_folder = fullfile(mfile_path(1:end-length('+autoware')), 'custom_msgs');
+custom_msgs_folder = fullfile(mfile_path(1:end-length('+autoware')), 'custom_msgs', 'matlab_gen');
 jar_folder = fullfile(custom_msgs_folder, 'jar');
-jar_file_locations{1} = sprintf('%s\\autoware_msgs-1.8.0.jar', jar_folder);
-jar_file_locations{2} = sprintf('%s\\jsk_recognition_msgs-1.2.5.jar', jar_folder);
-jar_file_locations{3} = sprintf('%s\\vector_map_msgs-1.8.0.jar', jar_folder);
+jar_file_locations{1} = fullfile(jar_folder, 'autoware_msgs-1.8.0.jar');
+jar_file_locations{2} = fullfile(jar_folder, 'dbw_mkz_msgs-1.8.0.jar');
+jar_file_locations{3} = fullfile(jar_folder, 'jsk_recognition_msgs-1.2.5.jar');
+jar_file_locations{4} = fullfile(jar_folder, 'vector_map_msgs-1.8.0.jar');
 
 javaclasspath_txt = fullfile(prefdir(), 'javaclasspath.txt');
 type = exist(javaclasspath_txt, 'file');
